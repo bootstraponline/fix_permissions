b47cbd08c2bd1d9e25ee11fe02e9a0e744536c71
bootstraponline <code@bootstraponline.com>
2015-07-06 12:33:50 -0400

    Use posix from string

--- src/com/bootstraponline/Visitor.java
+++ src/com/bootstraponline/Visitor.java
@@ -8,47 +8,28 @@ import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.nio.file.attribute.PosixFilePermission;
 import java.nio.file.attribute.PosixFilePermissions;
-import java.util.HashSet;
 import java.util.Set;
 
 /*
+Default OS X permissions:
 
-default OS X permissions:
 
--rw-r--r--   1 user  wheel    1 Jul  6 12:18 ok.txt --- file   - 644
 drwxr-xr-x   2 user  wheel   68 Jul  6 12:17 tmp    --- folder - 755
-
+-rw-r--r--   1 user  wheel    1 Jul  6 12:18 ok.txt --- file   - 644
 */
 
 public class Visitor extends SimpleFileVisitor<Path> {
-
     private static Set<PosixFilePermission> dirPermission;
     private static Set<PosixFilePermission> filePermission;
 
     static {
+        // r = 4, w = 2, x = 1
 
-        // Directory 755
-        dirPermission = new HashSet<>();
-        // Owner - RWE - 7
-        dirPermission.add(PosixFilePermission.OWNER_READ); // 4
-        dirPermission.add(PosixFilePermission.OWNER_WRITE); // 2
-        dirPermission.add(PosixFilePermission.OWNER_EXECUTE); // 1
-        // Group - RE - 5
-        dirPermission.add(PosixFilePermission.GROUP_READ); // 4
-        dirPermission.add(PosixFilePermission.GROUP_EXECUTE); // 1
-        // Other - RE - 5
-        dirPermission.add(PosixFilePermission.OTHERS_READ); // 4
-        dirPermission.add(PosixFilePermission.OTHERS_EXECUTE); // 1
+        // Directory | rwx r-x r-x | 755
+        dirPermission = PosixFilePermissions.fromString("rwx" + "r-x" + "r-x");
 
-        // File 644
-        filePermission = new HashSet<>();
-        // Owner - RW - 6
-        filePermission.add(PosixFilePermission.OWNER_READ); // 4
-        filePermission.add(PosixFilePermission.OWNER_WRITE); // 2
-        // Group - R - 4
-        filePermission.add(PosixFilePermission.GROUP_READ); // 4
-        // Other - R - 4
-        filePermission.add(PosixFilePermission.OTHERS_READ); // 4
+        // File | rw- r-- r-- | 644
+        filePermission = PosixFilePermissions.fromString("rw-" + "r--" + "r--");
     }
 
     @Override
